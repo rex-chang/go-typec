757d359495a6d16c5904fc2c0e67c131a0a91e17
rex-chang <swordwater@gmail.com>
2018-02-02 16:34:29 +0800

    提高测试覆盖率

--- typec_test.go
+++ typec_test.go
@@ -9,6 +9,10 @@ import (
 var inFloat32 = float32(1234.56)
 var inFloat64 = float64(1234.56)
 
+var inStruct = struct {
+	t string
+}{t: "123"}
+
 func TestInt2String(t *testing.T) {
 	//in test
 	var in = 123
@@ -23,6 +27,7 @@ func TestInt2String(t *testing.T) {
 	outInt8, _ := In(inInt8).String()
 	outUint8, _ := In(inUint8).String()
 	outString, _ := In(inString).String()
+	_, err := In(inStruct).String()
 	assert.Equal(t, "123", out, "convert error ")
 	assert.Equal(t, "1234.56", outFloat32Ret, " float32 convert error")
 	assert.Equal(t, "1234.56", outFloat64Ret, " float64 convert error")
@@ -30,6 +35,7 @@ func TestInt2String(t *testing.T) {
 	assert.Equal(t, "127", outInt8, " int convert error")
 	assert.Equal(t, "244", outUint8, " uint convert error")
 	assert.Equal(t, "123a", outString, " string convert error")
+	assert.Error(t, err)
 }
 
 func TestAny2Int(t *testing.T) {
@@ -45,9 +51,12 @@ func TestAny2Int(t *testing.T) {
 	outStr, _ := In(inString).Int()
 	outFloat32Ret, _ := In(inFloat32).Int()
 	outFloat64Ret, _ := In(inFloat64).Int()
+	_, err := In(inStruct).Int()
 	assert.Equal(t, expect, outUint, "convert error ")
 	assert.Equal(t, expect, outStr, "string convert error")
 	assert.Equal(t, expect, outInt8, "string convert error")
 	assert.Equal(t, int(1234), outFloat32Ret, "string convert error")
 	assert.Equal(t, int(1234), outFloat64Ret, "string convert error")
+	assert.Error(t, err)
+
 }
